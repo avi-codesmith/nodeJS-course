4be869159aa30d9399aecdf2c2104b5667d34982
avi-codesmith <avi.codesmith@gmail.com>
2026-07-22 19:06:12 +0530

    All API request in practise

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -58,7 +58,7 @@ app.use(express.json());
 
 //NATOURS CODE ==========================================================================================================================
 
-const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+let tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 // We have to convert parse the data into Obj, bcz streams it is in text format
 
 app.get('/api/v1/tours', (req, res) => {
@@ -109,6 +109,65 @@ app.post('/api/v1/tours', (req, res) => {
     },
   );
 }); // hanlde post request
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      data: `Invalid ID - ${id}`,
+    });
+  }
+
+  const newData = req.body;
+  const reqTour = tours.find((tour) => tour.id === id);
+
+  const updatedTour = Object.assign(reqTour, newData);
+
+  tours.push(updatedTour);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return console.log(err);
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: updatedTour,
+        },
+      });
+    },
+  );
+}); // hanlde patch request
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      data: `Invalid ID - ${id}`,
+    });
+  }
+
+  tours = tours.filter((tour) => tour.id !== id);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return console.log(err);
+
+      res.status(201).json({
+        status: 'success',
+        message: `Given tour has been deleted, id = ${id}`,
+      });
+    },
+  );
+}); // hanlde delete request
 
 const port = 8000;
 
